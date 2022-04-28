6c1a44529e0f67a259a79ee5247c3d1f935cc472
theokrueger <theokrueger@airmail.cc>
2022-04-28 17:16:14 -0400

    hacky rewrite to make it work in mp

--- fading-ammobox-contours/text.lua
+++ fading-ammobox-contours/text.lua
@@ -1,16 +1,41 @@
-local player = managers.player:player_unit()
-if alive(player) then
-        local pickups = World:find_units_quick("all", 23)
-        local mvec3_distance = mvector3.distance
-        for _, unit in ipairs(pickups) do
-		if alive(unit) and unit:pickup() then
-                        local distance = mvec3_distance(player:position(), unit:position())
-                        log(distance)
-                        if distance > 2000 then
-                                unit:pickup()._unit:contour():_upd_opacity(0)
-                        else
-                                unit:pickup()._unit:contour():_upd_opacity(100)
-                        end
-		end
-	end
+-- run this file only once
+if _G.boxes then
+        return
 end
+
+_G.boxes = {}
+
+Hooks:PostHook(
+        ContourExt,
+        'update',
+        'FAC_CE_update',
+        function(self, unit, t, dt)
+                local player = managers.player:player_unit()
+                if not alive(player) then return end
+                local mvec3_distance = mvector3.distance
+                for k, v in ipairs(boxes) do
+                        if not v._active then
+                                boxes[k] = nil
+                        else
+                                local distance = mvec3_distance(player:position(), v:position())
+
+                                if distance > 2000 then
+                                        v:pickup()._unit:contour():_upd_opacity(0)
+                                else
+                                        v:pickup()._unit:contour():_upd_opacity(100)
+                                end
+                        end
+                end
+
+        end
+)
+
+Hooks:PostHook(
+        AmmoClip,
+        'init',
+        'FAC_AC_init',
+        function(self, unit)
+                table.insert(boxes, unit)
+        end
+)
+
